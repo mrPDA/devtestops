eafb4c97ae3794c726958f8e5a90626a4ff5de46
mrPDA <sharxan@gmail.com>
2023-11-16 00:02:46 +0300

    Upd
    Del file
    New file

---
+++ bot_test.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def start(update, context):
+    update.message.reply_text('Привет! Я ваш эхо-бот.')
+
+def echo(update, context):
+    received_text = update.message.text
+    update.message.reply_text(received_text)
+
+def main():
+    TOKEN = 'ВАШ_ТОКЕН_ЗДЕСЬ'
+
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
