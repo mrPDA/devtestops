0d6b975612fedfddfc432f6d938543b7fcf09f41
mrPDA <sharxan@gmail.com>
2023-11-16 00:21:23 +0300

    www

--- bot_test.py
+++ bot_test.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 def start(update, context):
-    update.message.reply_text('Привет! Я ваш эхо-бот Раз дав три четыре.')
+    update.message.reply_text('Привет! Я ваш эхо-бот Раз дав три четыре 6.')
 
 def echo(update, context):
     received_text = update.message.text
